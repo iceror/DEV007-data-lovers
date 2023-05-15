7fb1de79384b137ba8f82922324dbb136d1c8083
iceror <ilsecervor@gmail.com>
2023-05-15 09:20:24 -0600

    add img files && fetch function

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { example } from './data.js';
 
-import data from './data/got/got.js';
-console.log(example, data.got);
-console.log('hola ');
+//import data from './data/got/got.js';
+
+function getCharacters() {
+  fetch('./data/got/got.json')
+    .then((response) => response.json())
+    .then((json) => {return json});
+}
+
+getCharacters();
