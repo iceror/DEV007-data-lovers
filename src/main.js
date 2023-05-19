348429a31879acf35d3e84b492432b7cc7265460
Yura <yurandreac@gmail.com>
2023-05-19 10:56:42 -0500

    crear botones filtra,js

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import dataGot from "./data/got/got.json" assert { type: "json" };
 //console.log(dataGot);
 
 document.getElementById("filteredCards");
+
+let createCards = dataGot.got.map((character) => {
+  const card = document.createElement("div");
+  card.firstName = "character-card";
+  card.innerHTML = `
+  
+`;
+});
