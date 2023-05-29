00dacdced8ad26b9b89a74e7c3dfad5d9d8ea98c
iceror <ilsecervor@gmail.com>
2023-05-29 12:54:02 -0600

    delete commented object

--- test/data.spec.js
+++ test/data.spec.js
@@ -50,40 +50,5 @@ describe('computeStats', () => {
         'House Targaryen': 1
       }
     });
-
-    // expect(result).toEqual({
-    //   lastNameCount: {
-    //     'Baelish': 1, 'Baratheon': 5, 'Bolton': 2, 'Clegane': 1, 'Drogo': 1,
-    //     'Giantsbane': 1, 'Grand Maester': 1, 'Greyjoy': 3, 'H ghar': 1, 'Hodor': 1, 'Lannister': 4, 'Martell': 1,
-    //     'Mormont': 2, 'Naharis': 1, 'Sand': 1, 'Seaworth': 1, 'Snow': 1, 'Sparrow': 1, 'Stark': 8, 'Targaryen': 2,
-    //     'Tarly': 1, 'Tarth': 1, 'The Red Woman': 1, 'Tyrell': 2, 'Unknown': 7, 'Worm': 1
-    //   },
-    //   houseCount: {
-    //     'Bronn': 1, 'Free Folk': 2, 'House Baelish': 1,
-    //     'House Baratheon': 5,
-    //     'House Bolton': 2,
-    //     'House Clegane': 1,
-    //     'House Greyjoy': 3,
-    //     'House Lannister': 4,
-    //     'House Lorath': 1,
-    //     'House Mormont': 2,
-    //     'House Naathi': 1,
-    //     'House Naharis': 1,
-    //     'House Sand': 1,
-    //     'House Seaworth': 1,
-    //     'House Stark': 10,
-    //     'House Targaryen': 3,
-    //     'House Tarly': 1,
-    //     'House Tarth': 1,
-    //     'House Tyrell': 2,
-    //     'House Unknown': 5,
-    //     'House Viper': 1,
-    //     'Lorathi': 1,
-    //     'Sparrow': 1,
-    //     'Worm': 1,
-    //   }
-    // });
-
-    //expect(getCharacters).toHaveBeenCalledTimes(1);
   });
 });
